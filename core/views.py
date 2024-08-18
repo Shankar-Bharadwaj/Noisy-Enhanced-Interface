855297a6decea3177c85dcbcba2f0cebef8fdbdd
Shankar Bharadwaj <bharadwajsankara@gmail.com>
2024-08-19 01:44:37 +0530

    connection setup with firebase rtdb

--- core/views.py
+++ core/views.py
@@ -10,7 +10,23 @@ def get_user_details(request):
         user_email = request.POST.get('email')
         request.session['user_name'] = user_name
         request.session['user_email'] = user_email
-        return render(request, 'audio_player.html', {'user_name': user_name})
+        audio_dir = os.path.join('media', 'audio')
+        folders = sorted(os.listdir(audio_dir))
+        folders.remove('.DS_Store')
+
+        audio_files = []
+        for folder in folders:
+            folder_path = os.path.join(audio_dir, folder)
+            if os.path.isdir(folder_path):
+                files = sorted(os.listdir(folder_path))
+                audio_files.append([os.path.join(folder, f) for f in files])
+
+        return render(request, 'audio_player.html', {
+            'audio_files': audio_files,
+            'user_name': request.session.get('user_name'),
+            'user_email': request.session.get('user_email'),
+            'MEDIA_URL': settings.MEDIA_URL
+        })
 
     # if return != 'POST'
     return render(request, 'get_user_details.html')
@@ -40,13 +56,19 @@ def submit_response(request):
     if request.method == "POST":
         user_name = request.session.get('user_name')
         user_email = request.session.get('user_email')
-        selected_option = request.POST.get('option')
-        audio_set_index = request.POST.get('audio_set_index')
 
-        ref = db.reference(f'responses/{user_email}/set_{audio_set_index}')
+        # Iterate through all audio sets and collect responses
+        responses = {}
+        for key, value in request.POST.items():
+            if key.startswith('option_'):
+                audio_set_index = key.split('_')[1]
+                responses[f'set_{audio_set_index}'] = value
+
+        # Reference to the Firebase RTDB
+        ref = db.reference(f'responses/{user_name}')
         ref.set({
             'user_name': user_name,
-            'selected_option': selected_option
+            'responses': responses
         })
 
         return render(request, 'thank_you.html')
