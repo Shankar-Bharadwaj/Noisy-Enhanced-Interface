1da834856c90f6bed26f82026a5fbba50a2cf24c
Shankar Bharadwaj <bharadwajsankara@gmail.com>
2024-08-19 03:57:24 +0530

    Deployment misc changes

--- survey/settings.py
+++ survey/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['noisy-enhanced-interface.onrender.com']
 
 
 # Application definition
